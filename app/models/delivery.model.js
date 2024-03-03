b11530be0bf003064ff035bc034ab4ec59598707
phtchung <phamthanhchung2001@gmail.com>
2024-03-03 19:17:37 +0700

    api tra hang cua user

--- app/models/delivery.model.js
+++ app/models/delivery.model.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose')
 
 const Delivery = mongoose.model(
-  'Delivery',
-  new mongoose.Schema(
-    {
-      name: String,
-      address: String,
-      phone: String,
-      status: {
-        type: Number,
-        enum: [5, 6, 7, 8, 9, 10, 11, 12],
-        default: 5
-      },
-      note: String,
-      completed_time: Date,
-      return_time: Date,
-      delivery_start_time: Date,
-        confirm_time:Date,
-      payment_method: String
-    },
-    { timestamps: true }
-  )
+    'Delivery',
+    new mongoose.Schema(
+        {
+            name: String,
+            address: String,
+            phone: String,
+            status: {
+                type: Number,
+                enum: [5, 6, 7, 8, 9, 10, 11, 12],
+                default: 5
+            },
+            note: String,
+            completed_time: Date,
+            return_time: Date,
+            delivery_start_time: Date,
+            confirm_time: Date,
+            payment_method: String,
+            return_reason: String,
+            return_image_list: [String],
+            return_video: String,
+        },
+        {timestamps: true}
+    )
 )
 
 module.exports = Delivery
--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,6 +1,8 @@
 const {authJwt} = require('../middlewares')
 const controller = require('../controllers/product.controller')
+const multer = require("multer");
 
+const upload = multer();
 module.exports = function (app) {
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept')
@@ -26,5 +28,8 @@ module.exports = function (app) {
     app.get('/product/req/:productId', [authJwt.verifyToken], controller.getReqOrderDetail)
 
     app.post('/product/updateStatus', [authJwt.verifyToken], controller.updateByWinner)
+
+    app.post('/user/returnProduct', [authJwt.verifyToken],upload.array('files[]',14), controller.UserReturnProduct)
+
 
 }
